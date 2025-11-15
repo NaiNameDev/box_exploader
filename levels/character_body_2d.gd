7d46e216f39597b8eaa02ae19d9e067c1081f9fe
shpakov08-cmd <shpakov08@internet.ru>
2025-11-15 22:08:07 +0300

    box
    ads

--- levels/character_body_2d.gd
+++ levels/character_body_2d.gd
@@ -5,10 +5,10 @@ extends CharacterBody2D
 
 func _physics_process(delta):
   # Получаем input vector (ввод игрока)
-  var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+  var direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
   
   # Рассчитываем новую скорость
   velocity = direction * speed
   
-  # Применяем движение и обрабатываем столкновенияs
+  # Применяем движение и обрабатываем столкновения
   move_and_slide()
