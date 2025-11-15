681eee532a3f71ec7bb765d31a3868a99e27379d
mocha-hacha <tqwer6576@gmail.com>
2025-11-15 22:10:10 +0300

    qw
    das

--- levels/area_2d.gd
+++ levels/area_2d.gd
@@ -1,1 +1,8 @@
 extends Area2D
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+func _on_body_entered(body):
+	if body.is_in_group("player"):
+		print("Игрок вошел в зону!")
--- levels/character_body_2d.gd
+++ levels/character_body_2d.gd
@@ -2,10 +2,11 @@ extends CharacterBody2D
 
 # Скорость движения персонажа в пикселях в секунду
 @export var speed: int = 300
+var have_box = false 
 
 func _physics_process(delta):
   # Получаем input vector (ввод игрока)
-  var direction: Vector2 = Input.get_vector("left", "right", "up", "down")
+  var direction: Vector2 = Input.get_vector("left","right","up","down")
   
   # Рассчитываем новую скорость
   velocity = direction * speed
